97a1c784f57d384956205d71a2c662c0c81e94ec
crackerjackmack1 <44295981+crackerjackmack1@users.noreply.github.com>
2018-11-18 08:51:34 -0600

    Update report-version.js

--- JS/report-version.js
+++ JS/report-version.js
@@ -1,6 +1,6 @@
-/* JavaScript: REPORT-VERSION.JS v1.0
+/* 
+ * JavaScript: REPORT-VERSION.JS v1.0
  * Author: J.G. McCracken
- * Email: j.g.mccracken65@gmail.com
  */
 
 var pgtitle=document.getElementsByTagName("TITLE");
